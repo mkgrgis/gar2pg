1a4a07160903e64e8198e39e1c6b6956dfbbce6d
Михаил <41448637+mkgrgis@users.noreply.github.com>
2026-02-12 16:27:02 +0300

    Перераспределение тестов

--- test/db_test.sh
+++ test/db_test.sh
@@ -1,19 +1,3 @@
-mkdir test/result;
-
-echo -n "ФАЙЛЫ ГАР  - ";
-echo 'select "№", xsd_id, loading_session_id, xsd_filename, xml_file_prefix, table_name, root_node, singular_transport_node, xsd_descr_general, xsd_descr_singular
-from xsd.transport_files;' | psql > test/result/xml_files.out;
-diff test/expected/xml_files.out test/result/xml_files.out > test/xml_files.diff;
-diff test/expected/xml_files.out test/result/xml_files.out || r="$?";
-echo "$r";
-
-echo -n "АТРИБУТЫ ГАР - ";
-echo 'select xsd_id, loading_session_id, xsd_filename, root_node, "№", transport_attribute, column_name, xsd_dt, "usage", "not null", "type", "cardinality", "length", name0, table_name, xml_file_prefix
-from xsd.transport_attributes;' | psql > test/result/xml_att.out;
-diff test/expected/xml_att.out test/result/xml_att.out > test/xml_att.diff;
-diff test/expected/xml_att.out test/result/xml_att.out || r="$?";
-echo "$r";
-
 echo -n "DDL - "
 ddf='sql/ГАР_pgDDL.sql';
 cat "$ddf" | psql -e > test/result/ddl.out || true;
