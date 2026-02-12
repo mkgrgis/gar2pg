5b294844c7c7df910511529dec63c8daf25b7b69
Михаил <41448637+mkgrgis@users.noreply.github.com>
2026-02-12 16:33:03 +0300

    Уточнение тестов

--- test/db_test.sh
+++ test/db_test.sh
@@ -1,17 +1,3 @@
-echo -n "DDL - "
-ddf='sql/ГАР_pgDDL.sql';
-cat "$ddf" | psql -e > test/result/ddl.out || true;
-diff test/expected/ddl.out test/result/ddl.out > test/ddl.diff;
-diff test/expected/ddl.out test/result/ddl.out || r="$?";
-echo "$r";
-
-echo -n "Сопоставление словарей PostgreSQL и xsd - "
-dct='sql/dict_xml_pg.sql';
-cat "$dct" | psql -e > test/result/dict_xml_pg.out || true;
-diff test/expected/dict_xml_pg.out test/result/dict_xml_pg.out > test/dict_xml_pg.diff;
-diff test/expected/dict_xml_pg.out test/result/dict_xml_pg.out || r="$?";
-echo "$r";
-
 # Пример прогрузки местных данных
 xmldatadir='test/ГАР XML/09';
 ls -1 "$xmldatadir" | while read f; do ./load_xml_ГАР.sh "$xmldatadir/$f"; done;
